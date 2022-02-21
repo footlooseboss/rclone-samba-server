fda94363067f7eab40e67b665587a4c2dee7c76c
Patrick Hütter <p.huetter@encircle360.com>
2022-02-21 11:15:14 +0100

    fixed rclone mount script

--- rclone-mount.sh
+++ rclone-mount.sh
@@ -4,7 +4,7 @@ if [[ -z "${RCLONE_MOUNT_PARAMS}" ]]; then
   /usr/bin/rclone --config=/rclone/config/rclone.conf --cache-dir=/rclone/cache mount remote: /mnt/remote --vfs-cache-mode writes --allow-other --allow-non-empty
 else
     echo "Running rclone with custom params from env variable RCLONE_MOUNT_PARAMS: ${RCLONE_MOUNT_PARAMS}"
-  /usr/bin/rclone --config=/rclone/config/rclone.conf --cache-dir=/rclone/cache mount remote: /mnt/remote "${RCLONE_MOUNT_PARAMS}"
+  /usr/bin/rclone --config=/rclone/config/rclone.conf --cache-dir=/rclone/cache mount remote: /mnt/remote ${RCLONE_MOUNT_PARAMS}
 fi
 
 
